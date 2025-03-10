5be73afae64fea4f53e128bda22e5bde49d9fecb
CloudManPro <cloudmanpro111@gmail.com>
2025-03-10 18:04:00 -0300

    Add files via upload

--- EC2/Scripts/WordpressCopy.sh
+++ EC2/Scripts/WordpressCopy.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
-# Carregar variáveis de ambiente do arquivo .env
+# Carregar variáveis de ambiente do arquivo .env e converter as chaves para maiúsculas
 while IFS='=' read -r key value; do
+    key=$(echo "$key" | tr '[:lower:]' '[:upper:]')
     export "$key=$value"
 done </home/ec2-user/.env
 
-#Tenta acesso a internet por 10x a cada 30s. Objetivo é aguardar a inicialização, se houver alguma dependencia de alguma
-#instância, como um NAT por exemplo,  para ter acesso a internet.
-max_attempts=1000
-wait_time=4
-test_address="https://www.google.com"
+# Tenta acesso à internet por 1000 tentativas, esperando 4 segundos entre elas.
+MAX_ATTEMPTS=1000
+WAIT_TIME=4
+TEST_ADDRESS="https://www.google.com"
 test_connectivity() {
-    for attempt in $(seq 1 $max_attempts); do
-        echo "Tentativa $attempt de $max_attempts: Testando a conectividade com $test_address..."
-        if curl -I $test_address >/dev/null 2>&1; then
+    for attempt in $(seq 1 $MAX_ATTEMPTS); do
+        echo "Tentativa $attempt de $MAX_ATTEMPTS: Testando a conectividade com $TEST_ADDRESS..."
+        if curl -I $TEST_ADDRESS >/dev/null 2>&1; then
             echo "Conectividade com a Internet estabelecida."
             return 0
         else
-            echo "Conectividade com a Internet falhou. Aguardando $wait_time segundos para a próxima tentativa..."
-            sleep $wait_time
+            echo "Conectividade com a Internet falhou. Aguardando $WAIT_TIME segundos para a próxima tentativa..."
+            sleep $WAIT_TIME
         fi
     done
     return 1
 }
 test_connectivity
 
-# Inicia o servidor Apache e configura para iniciar na inicialização
+# Inicia o servidor Apache e configura-o para iniciar na inicialização
 sudo systemctl start httpd
 sudo systemctl enable httpd
 
-# Definição das variáveis
-EC2NAME=$Name
-SECRET_NAME_ARN=$aws_secretsmanager_secret_version_Source_ARN_0
-DBNAME=$aws_db_instance_Target_Name_0
-SECRETREGION=$aws_secretsmanager_secret_version_Source_Region_0
-RDS_ENDPOINT=$aws_db_instance_Target_Endpoint_0
-# Extração do endereço e da porta do endpoint
+# Definição das variáveis (todas em uppercase)
+EC2NAME=$NAME
+SECRET_NAME_ARN=$AWS_SECRETSMANAGER_SECRET_VERSION_SOURCE_ARN_0
+DBNAME=$AWS_DB_INSTANCE_TARGET_NAME_0
+SECRETREGION=$AWS_SECRETSMANAGER_SECRET_VERSION_SOURCE_REGION_0
+RDS_ENDPOINT=$AWS_DB_INSTANCE_TARGET_ENDPOINT_0
+
+# Extração do endereço (sem a porta) do endpoint
 ENDPOINT_ADDRESS=$(echo $RDS_ENDPOINT | cut -d: -f1)
 
-if [ ! -z "$aws_secretsmanager_secret_version_Source_ARN_0" ] && [ "$aws_secretsmanager_secret_version_Source_ARN_0" != "none" ]; then
+if [ ! -z "$AWS_SECRETSMANAGER_SECRET_VERSION_SOURCE_ARN_0" ] && [ "$AWS_SECRETSMANAGER_SECRET_VERSION_SOURCE_ARN_0" != "none" ]; then
     # Recupera os valores dos segredos do AWS Secrets Manager
-    SOURCE_NAME_VALUE=$(aws secretsmanager get-secret-value --secret-id $aws_secretsmanager_secret_version_Source_ARN_0 --query 'SecretString' --output text --region $SECRETREGION)
+    SOURCE_NAME_VALUE=$(aws secretsmanager get-secret-value --secret-id $AWS_SECRETSMANAGER_SECRET_VERSION_SOURCE_ARN_0 --query 'SecretString' --output text --region $SECRETREGION)
     DB_USER=$(echo $SOURCE_NAME_VALUE | jq -r .username)
     DB_PASSWORD=$(echo $SOURCE_NAME_VALUE | jq -r .password)
 else
@@ -49,10 +50,10 @@ else
     DB_PASSWORD="TypeNewPassword"
 fi
 
-# Verifica se o EFS ARN existe e é diferente de none
-if [ ! -z "$aws_efs_file_system_Target_ARN_0" ] && [ "$aws_efs_file_system_Target_ARN_0" != "none" ]; then
+# Verifica se o EFS ARN existe e é diferente de "none"
+if [ ! -z "$AWS_EFS_FILE_SYSTEM_TARGET_ARN_0" ] && [ "$AWS_EFS_FILE_SYSTEM_TARGET_ARN_0" != "none" ]; then
     # Montagem do EFS
-    EFS_TARGET_ARN=$aws_efs_file_system_Target_ARN_0
+    EFS_TARGET_ARN=$AWS_EFS_FILE_SYSTEM_TARGET_ARN_0
     sudo mkdir -p /var/www/html
     sudo yum install -y amazon-efs-utils
     sudo mount -t efs $EFS_TARGET_ARN:/ /var/www/html
@@ -74,24 +75,24 @@ sudo find /var/www/html/ -type d -exec sudo chmod 755 {} \;
 sudo find /var/www/html/ -type f -exec sudo chmod 644 {} \;
 
 # Testa a disponibilidade da RDS
-max_attempts_rds=30
-wait_time_rds=10
-rds_ready=0
+MAX_ATTEMPTS_RDS=30
+WAIT_TIME_RDS=10
+RDS_READY=0
 
 echo "Verificando a disponibilidade da RDS... $ENDPOINT_ADDRESS, $DB_USER, $DBNAME"
-for attempt in $(seq 1 $max_attempts_rds); do
+for attempt in $(seq 1 $MAX_ATTEMPTS_RDS); do
     if mysql -u "$DB_USER" -p"$DB_PASSWORD" -h "$ENDPOINT_ADDRESS" -e "SHOW DATABASES;" >/dev/null 2>&1; then
         echo "RDS disponível."
-        rds_ready=1
+        RDS_READY=1
         break
     else
-        echo "RDS ainda não disponível $DB_USER $ENDPOINT_ADDRESS. Tentativa $attempt de $max_attempts_rds. Aguardando $wait_time_rds segundos..."
-        sleep $wait_time_rds
+        echo "RDS ainda não disponível $DB_USER $ENDPOINT_ADDRESS. Tentativa $attempt de $MAX_ATTEMPTS_RDS. Aguardando $WAIT_TIME_RDS segundos..."
+        sleep $WAIT_TIME_RDS
     fi
 done
 
-if [ $rds_ready -ne 1 ]; then
-    echo "RDS não ficou disponível após $max_attempts_rds tentativas. Interrompendo a instalação."
+if [ $RDS_READY -ne 1 ]; then
+    echo "RDS não ficou disponível após $MAX_ATTEMPTS_RDS tentativas. Interrompendo a instalação."
     exit 1
 fi
 
