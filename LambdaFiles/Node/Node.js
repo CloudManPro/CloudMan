4251029639b915e59f9a47276e0b7892e44284fd
CloudManPro <cloudmanpro111@gmail.com>
2026-01-08 22:25:57 -0300

    Update Node.js

--- LambdaFiles/Node/Node.js
+++ LambdaFiles/Node/Node.js
@@ -1,5 +1,5 @@
-Hexports.handler = async (event) => {
-    // TODO: Implement your logic here abcd
+exports.handler = async (event) => {
+    // TODO: Implement your logic here abcd EFGH
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from CloudMan (Node.js)!'),
