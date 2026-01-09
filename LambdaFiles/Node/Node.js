37d07b8ff9e97594102de3d281c72c0d102ee3ad
CloudManPro <cloudmanpro111@gmail.com>
2026-01-08 22:30:54 -0300

    Delete LambdaFiles/Node directory

--- LambdaFiles/Node/Node.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    // TODO: Implement your logic here abcd EFGH
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from CloudMan (Node.js)!'),
-    };
-    return response;
-};
