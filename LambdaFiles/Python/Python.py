3c01543bfe262cf1949bf3726bfc200abae38707
CloudManPro <cloudmanpro111@gmail.com>
2026-01-08 22:31:05 -0300

    Delete LambdaFiles/Python directory

--- LambdaFiles/Python/Python.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    # TODO: Implement your logic here abcd EFGH
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from CloudMan (Python)!')
-    }
