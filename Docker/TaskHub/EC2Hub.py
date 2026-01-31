d4b64d3b7c162ef094170376048c8599589475f7
CloudManPro <cloudmanpro111@gmail.com>
2026-01-30 22:09:27 -0300

    Update EC2Hub.py

--- Docker/TaskHub/EC2Hub.py
+++ Docker/TaskHub/EC2Hub.py
@@ -19,7 +19,7 @@ import time # Importar time para a lógica de espera
 
 # (O restante das importações e configurações iniciais permanecem as mesmas)
 # ...
-#v4 2
+#v4 3
 try:
     import watchtower
 except ImportError:
